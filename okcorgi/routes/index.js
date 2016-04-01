775fae28a984b5609cdd8555fd88c58cd9afc956
Kelly Everett <dkever79@yahoo.com>
2016-04-01 14:04:02 -0500

    fixed db get data

--- okcorgi/routes/index.js
+++ okcorgi/routes/index.js
@@ -4,9 +4,11 @@ var Corgi = require('../models/corgi');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Corgi.findOne({ _id: '56fead1ae4b0281bef93a752' }, 'name', function(err, corgi) {
+  Corgi.findOne({ name: 'Bob' }, 'name age url', function(err, corgi) {
     if (err) console.log(err);
     console.log(corgi);
+
+  res.render('index', { title: 'Express' });
   });
 });
 
